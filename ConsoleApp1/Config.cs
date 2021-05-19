12a3b1b0ddcee82b89645dff755e8ef5a4697a1f
pjaipakdee19 <jai.peerapong@gmail.com>
2021-05-20 02:04:27 +0700

    Add the console app to import the csv and export to json

---
+++ ConsoleApp1/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Config
+    {
+        public string json_dispense_log_path { get; set; }
+    }
+}
