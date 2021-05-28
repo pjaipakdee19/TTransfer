6fda191ad851803db36312d5a70887669eba3aa4
pjaipakdee19 <jai.peerapong@gmail.com>
2021-05-29 01:14:18 +0700

    Implement timer class to service

--- TAService/Service1.cs
+++ TAService/Service1.cs
@@ -23,6 +23,8 @@ namespace TAService
         {
             MissingFieldFound = null,
         };
+
+        public DateTime randomStartTime = new DateTime();
         public Service1()
         {
             InitializeComponent();
@@ -30,26 +32,87 @@ namespace TAService
 
         protected override void OnStart(string[] args)
         {
+            
+            
+            System.Timers.Timer timScheduledTask = new System.Timers.Timer();
+            timScheduledTask.Enabled = true;
+            timScheduledTask.Interval = 60 * 1000;
+            timScheduledTask.Elapsed += new System.Timers.ElapsedEventHandler(timScheduledTask_Elapsed);
+            //try
+            //{
+            //    Logger.Info("Hello world");
+            //    Logger.Warn("Test Warn");
+            //    System.Console.ReadKey();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Logger.Error(ex, "Goodbye cruel world");
+            //}
+
+        }
+
+        protected override void OnStop()
+        {
+            NLog.LogManager.Shutdown();
+        }
+
+        void timScheduledTask_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var utcTime = DateTime.UtcNow;
+            var ictZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+            var actualTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, ictZone);
+            // Execute some task
+            bool isBetweenStartTime = check_service_starttime(actualTime);
+            if(isBetweenStartTime && (randomStartTime == (new DateTime())))
+            {
+                //Random time
+                Random r = new Random();
+                int genRand = r.Next(0, 25);
+                randomStartTime = randomStartTime.AddMinutes(genRand);
+                Logger.Info("Start Time will be : " + randomStartTime);
+            }
+            if ((actualTime >= randomStartTime) && (randomStartTime != (new DateTime())))
+            {
+                //Go working extract log and send process
+                try
+                {
+                    startOperation();
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Exception on "+ex.ToString());
+                }
+
+
+                randomStartTime = new DateTime(); //Reset
+            }
+        }
+
+        bool check_service_starttime(DateTime actualTime)
+        {
+            // Here is the code we need to execute periodically
+            //random when 07:30
+            var startDatetime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 07, 30, 00); //!!! MOVE HARD CODE TO CONFIGURATION FILE
+            var tillDateTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 08, 00, 00); //!!! MOVE HARD CODE TO CONFIGURATION FILE
+
+            //if local timm diff between server time ? > 1 
+            //ExecuteCmd exe = new ExecuteCmd();
+            //exe.ExecuteCommandSync("net stop w32time");
+            //exe.ExecuteCommandSync("net stop w32time");
+            //exe.ExecuteCommandSync("net stop w32time");
+            //exe.ExecuteCommandSync("net stop w32time");
+            //exe.ExecuteCommandSync("net stop w32time");
+            //Reboot the PC after execute command !!!???!!!
+
+            return (actualTime > startDatetime && actualTime < tillDateTime);
+        }
+
+        void startOperation()
+        {
             //Init configuration variable
             string csv_history_path = ConfigurationManager.AppSettings.Get("csv_history_path");
-            string jsonDispenseLogPath = ConfigurationManager.AppSettings.Get("json_dispense_log_path"); 
+            string jsonDispenseLogPath = ConfigurationManager.AppSettings.Get("json_dispense_log_path");
             string csv_history_achive_path = ConfigurationManager.AppSettings.Get("csv_history_achive_path");
-
-            //Check Time
-
-            //Time met following the setup period (07:00AM)
-
-            //Random start time
-
-            //Current time == Random start time
-
-            //Check the time from server ????
-
-            //if Diff time between current time > servertime more than 1 min 
-
-            //else time < 1 min
-
-
             //find the csv in history files
             DirectoryInfo csvHistoryPathInfo = new DirectoryInfo(csv_history_path);
             foreach (var csvFile in csvHistoryPathInfo.GetFiles("*.csv"))
@@ -109,13 +172,22 @@ namespace TAService
                     //Y Did the files name ended with _p2 ? (Does it have file name not end with p2 ?)
                     if (!jsonFile.Name.Contains("_p2"))
                     {
-                        //send to api
+                        
+                        int retry = 1;
+                        while (retry <= 3)
+                        {
+                            //send to api
+                            
+                            //success mv change filename end with _p2
+                            //string[] mvFile = Directory.GetFiles(jsonFile.FullName);
+                            int extensionIndex = jsonFile.Name.IndexOf(".json");
 
-                        //success mv change filename end with _p2
-                        //string[] mvFile = Directory.GetFiles(jsonFile.FullName);
-                        int extensionIndex = jsonFile.Name.IndexOf(".json");
-
-                        File.Move(jsonFile.FullName, jsonDispenseLogPath + "\\" + jsonFile.Name.Substring(0, extensionIndex - 1) + "_p2.json");
+                            File.Move(jsonFile.FullName, jsonDispenseLogPath + "\\" + jsonFile.Name.Substring(0, extensionIndex - 1) + "_p2.json");
+                        }
+                        if (retry >3)
+                        {
+                            Logger.Error("Exception on send to api");
+                        }
                     }
                 }
                 catch (Exception ex)
@@ -124,29 +196,6 @@ namespace TAService
                 }
 
             }
-
-
-
-
-
-
-
-            //try
-            //{
-            //    Logger.Info("Hello world");
-            //    Logger.Warn("Test Warn");
-            //    System.Console.ReadKey();
-            //}
-            //catch (Exception ex)
-            //{
-            //    Logger.Error(ex, "Goodbye cruel world");
-            //}
-
-        }
-
-        protected override void OnStop()
-        {
-            NLog.LogManager.Shutdown();
         }
 
         private static string[] RemoveDuplicates(List<string> dateList)
