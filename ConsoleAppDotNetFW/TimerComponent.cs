a2a8ffbe347c3b97f43f9fb514c1a64e3fded79c
pjaipakdee19 <jai.peerapong@gmail.com>
2021-05-28 08:08:32 -0700

    Add timeer class

---
+++ ConsoleAppDotNetFW/TimerComponent.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+//using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ConsoleAppDotNetFW
+{
+    class TimerComponent
+    {
+        public static DateTime GetNetworkTime()
+        {
+            //default Windows time server
+            const string ntpServer = "th.pool.ntp.org";
+
+            // NTP message size - 16 bytes of the digest (RFC 2030)
+            var ntpData = new byte[48];
+
+            //Setting the Leap Indicator, Version Number and Mode values
+            ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+            //The UDP port number assigned to NTP is 123
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            //NTP uses UDP
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                socket.Connect(ipEndPoint);
+
+                //Stops code hang if NTP is blocked
+                socket.ReceiveTimeout = 3000;
+
+                socket.Send(ntpData);
+                socket.Receive(ntpData);
+                socket.Close();
+            }
+
+            //Offset to get to the "Transmit Timestamp" field (time at which the reply 
+            //departed the server for the client, in 64-bit timestamp format."
+            const byte serverReplyTime = 40;
+
+            //Get the seconds part
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+
+            //Get the seconds fraction
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+
+            //Convert From big-endian to little-endian
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+            //**UTC** time
+            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+
+            return networkDateTime.ToLocalTime();
+        }
+
+        // stackoverflow.com/a/3294698/162671
+        static uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
+        }
+
+        public static DateTime GetNetworkTime2()
+        {
+            const string NtpServer = "th.pool.ntp.org";
+
+            const int DaysTo1900 = 1900 * 365 + 95; // 95 = offset for leap-years etc.
+            const long TicksPerSecond = 10000000L;
+            const long TicksPerDay = 24 * 60 * 60 * TicksPerSecond;
+            const long TicksTo1900 = DaysTo1900 * TicksPerDay;
+
+            var ntpData = new byte[48];
+            ntpData[0] = 0x1B; // LeapIndicator = 0 (no warning), VersionNum = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+            var addresses = Dns.GetHostEntry(NtpServer).AddressList;
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            long pingDuration = Stopwatch.GetTimestamp(); // temp access (JIT-Compiler need some time at first call)
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                socket.Connect(ipEndPoint);
+                socket.ReceiveTimeout = 5000;
+                socket.Send(ntpData);
+                pingDuration = Stopwatch.GetTimestamp(); // after Send-Method to reduce WinSocket API-Call time
+
+                socket.Receive(ntpData);
+                pingDuration = Stopwatch.GetTimestamp() - pingDuration;
+            }
+
+            long pingTicks = pingDuration * TicksPerSecond / Stopwatch.Frequency;
+
+            // optional: display response-time
+            // Console.WriteLine("{0:N2} ms", new TimeSpan(pingTicks).TotalMilliseconds);
+
+            long intPart = (long)ntpData[40] << 24 | (long)ntpData[41] << 16 | (long)ntpData[42] << 8 | ntpData[43];
+            long fractPart = (long)ntpData[44] << 24 | (long)ntpData[45] << 16 | (long)ntpData[46] << 8 | ntpData[47];
+            long netTicks = intPart * TicksPerSecond + (fractPart * TicksPerSecond >> 32);
+
+            var networkDateTime = new DateTime(TicksTo1900 + netTicks + pingTicks / 2);
+
+            return networkDateTime.ToLocalTime(); // without ToLocalTime() = faster
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DateTime.now is local computer time " + DateTime.Now);
+
+            //var currentTime = GetNetworkTime();
+            //var time2 = GetNetworkTime2();
+            //Console.WriteLine("ServerTime from function " + currentTime);
+            //Console.WriteLine("ServerTime2 from function " + time2);
+
+            var utcTime = DateTime.UtcNow;
+            var cstZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+            var ictZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+            var actualTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, ictZone);
+            Console.WriteLine("UTC time from function " + utcTime);
+            Console.WriteLine("Converted Time " + actualTime);
+            var startDatetime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 07, 30, 00);
+            var tillDateTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 08, 00, 00);
+
+            //ExecuteCmd exe = new ExecuteCmd();
+            //exe.ExecuteCommandSync("net stop w32time");
+            //exe.ExecuteCommandSync("net stop w32time");
+            //exe.ExecuteCommandSync("net stop w32time");
+            //exe.ExecuteCommandSync("net stop w32time");
+            //exe.ExecuteCommandSync("net stop w32time");
+
+            Console.WriteLine("Start time " + startDatetime + "Till time " + tillDateTime);
+            string relationship;
+            int result = DateTime.Compare(actualTime, startDatetime);
+            if (result < 0)
+                relationship = "is earlier than";
+            else if (result == 0)
+                relationship = "is the same time as";
+            else
+                relationship = "is later than";
+            Console.WriteLine("{0} {1} {2}", actualTime, relationship, startDatetime);
+            //string test1 = System.Configuration.ConfigurationManager.AppSettings["Key1"];
+
+        }
+    }
+}
