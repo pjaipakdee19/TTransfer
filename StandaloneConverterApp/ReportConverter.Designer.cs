df48b4b698be0f9c8617f99bb028e589cc677974
pjaipakdee19 <jai.peerapong@gmail.com>
2021-07-24 00:18:52 +0700

    Initial converter app.

---
+++ StandaloneConverterApp/ReportConverter.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace StandaloneConverterApp
+{
+    partial class ReportConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.selectFilebtn = new System.Windows.Forms.Button();
+            this.outputBrowseDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.staticFilenameLbl = new System.Windows.Forms.Label();
+            this.fileNameLbl = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "OpenFileDialog";
+            // 
+            // selectFilebtn
+            // 
+            this.selectFilebtn.Location = new System.Drawing.Point(169, 220);
+            this.selectFilebtn.Name = "selectFilebtn";
+            this.selectFilebtn.Size = new System.Drawing.Size(170, 62);
+            this.selectFilebtn.TabIndex = 0;
+            this.selectFilebtn.Text = "Select File";
+            this.selectFilebtn.UseVisualStyleBackColor = true;
+            this.selectFilebtn.Click += new System.EventHandler(this.selectFilebtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(125, 162);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(255, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // staticFilenameLbl
+            // 
+            this.staticFilenameLbl.AutoSize = true;
+            this.staticFilenameLbl.Location = new System.Drawing.Point(104, 87);
+            this.staticFilenameLbl.Name = "staticFilenameLbl";
+            this.staticFilenameLbl.Size = new System.Drawing.Size(55, 13);
+            this.staticFilenameLbl.TabIndex = 2;
+            this.staticFilenameLbl.Text = "Filename :";
+            // 
+            // fileNameLbl
+            // 
+            this.fileNameLbl.AutoSize = true;
+            this.fileNameLbl.Location = new System.Drawing.Point(166, 86);
+            this.fileNameLbl.Name = "fileNameLbl";
+            this.fileNameLbl.Size = new System.Drawing.Size(33, 13);
+            this.fileNameLbl.TabIndex = 3;
+            this.fileNameLbl.Text = "None";
+            // 
+            // ReportConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(510, 385);
+            this.Controls.Add(this.fileNameLbl);
+            this.Controls.Add(this.staticFilenameLbl);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.selectFilebtn);
+            this.Name = "ReportConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ReportConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.Button selectFilebtn;
+        private System.Windows.Forms.FolderBrowserDialog outputBrowseDialog;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label staticFilenameLbl;
+        private System.Windows.Forms.Label fileNameLbl;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ StandaloneConverterApp/ReportConverter.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Newtonsoft.Json;
+using AutoTintLibrary;
+
+namespace StandaloneConverterApp
+{
+    public partial class ReportConverter : Form
+    {
+        public CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            MissingFieldFound = null,
+            HeaderValidated = null,
+            IgnoreBlankLines = true,
+            PrepareHeaderForMatch = args => args.Header.Replace(" ", "")
+
+        };
+        dynamic JsonSetting = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore
+        };
+        string csv_filepath = string.Empty;
+        string save_location = string.Empty;
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        public ReportConverter()
+        {
+            InitializeComponent();
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            Text = Text + " " + version.Major + "." + version.Minor;// + " (build " + version.Build + ")"; //change form title
+        }
+
+        private void selectFilebtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv (*.csv)|*.csv|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    csv_filepath = openFileDialog.FileName;
+                }
+            }
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                save_location = dialog.FileName;
+            }
+
+            if (String.IsNullOrEmpty(csv_filepath)) { MessageBox.Show($"Please select he csv file", "Error", MessageBoxButtons.OK); return; }
+            if (String.IsNullOrEmpty(save_location)) { MessageBox.Show($"Please select the output location", "Error", MessageBoxButtons.OK); return; }
+            //Read the contents of the file into a stream
+            MessageBox.Show($"File path : {csv_filepath} | Export path : {save_location}", "File Content at path: " + csv_filepath, MessageBoxButtons.OK);
+            Logger.Info($"Test Log File path : {csv_filepath} | Export path : {save_location}");
+        }
+
+        public async Task<string> convertCSV(string input_path)
+        {
+            string statusCode = string.Empty;
+            string responseMessage = string.Empty;
+            try
+            {
+                var reader = new StreamReader(input_path);
+                var csv = new CsvReader(reader, csvConfig);
+                var records = csv.GetRecords<DispenseHistory>().ToList();
+                records.RemoveAll(x => string.IsNullOrWhiteSpace(x.dispensed_formula_id));
+                var dateList = new List<string>();
+                for (int i = 0; i < records.Count(); i++)
+                {
+                    string[] date = records[i].dispensed_date.Split(' ');
+                    int year = int.Parse(date[0].Split('/')[2]);
+                    int now = DateTime.Today.Year;
+                    if (year > now)
+                    {
+                        date[0] = $"{date[0].Split('/')[1]}/{date[0].Split('/')[0]}/{year - 543}";
+                        records[i].dispensed_date = $"{date[0]} {date[1]}";
+                        dateList.Add(date[0]);
+                    }
+                }
+                string[] cleanDate = RemoveDuplicates(dateList);
+                //save the dispenselog to file.json following date
+                for (int i = 0; i < cleanDate.Count(); i++)
+                {
+                    var exportRecord = new List<DispenseHistory>();
+                    var exportRecordBI = new List<DispenseHistoryBI>();
+                    for (int j = 0; j < records.Count(); j++)
+                    {
+
+                        if (records[j].dispensed_date.Contains(cleanDate[i]))
+                        {
+                            exportRecord.Add(records[j]);
+                        }
+
+                    }
+                    if (exportRecord.Count > 0)
+                    {
+                        var export_path = $"{save_location}\\full_dispense_log_{cleanDate[i].Replace("/", "_")}.json";
+                        Logger.Info("Export log path : " + export_path);
+                        File.WriteAllText(export_path, JsonConvert.SerializeObject(exportRecord), Encoding.UTF8);
+                    }
+                    else
+                    {
+                        Logger.Info($"Doesn't have match dispense date in csv with cleanDate ({cleanDate[i].Replace("/", "_")})");
+                    }
+                }
+
+                return JsonConvert.SerializeObject(new { statusCode = statusCode, message = responseMessage });
+            }catch(Exception ex)
+            {
+                return JsonConvert.SerializeObject(new { statusCode = "Error", message = ex.Message });
+            }
+            
+        }
+
+        private static string[] RemoveDuplicates(List<string> dateList)
+        {
+            HashSet<string> set = new HashSet<string>(dateList);
+            string[] result = new string[set.Count];
+            set.CopyTo(result);
+            return result;
+        }
+        private void CreateDirectoryIfNotExist(string filepath)
+        {
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+            }
+        }
+    }
+}
+
+
+
