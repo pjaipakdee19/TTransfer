f211a2fcc3cf6c4e791df7ba0c3ebd01e898adbe
pjaipakdee19 <jai.peerapong@gmail.com>
2021-09-28 20:46:27 +0700

    Change version to 1.4

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -153,7 +153,7 @@ namespace IOTClient
                     AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
                     if (titleAttribute.Title != "")
                     {
-                        return $"{titleAttribute.Title} V1.3";
+                        return $"{titleAttribute.Title} V1.4";
                     }
                 }
                 return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().CodeBase);
