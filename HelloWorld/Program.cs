e51dabcf17d3d219ee9a7c6ba1ecd3f14ebb7fda
pjaipakdee19 <jai.peerapong@gmail.com>
2021-11-03 22:55:37 +0700

    Update version number to 1.6

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -153,7 +153,7 @@ namespace IOTClient
                     AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
                     if (titleAttribute.Title != "")
                     {
-                        return $"{titleAttribute.Title} V1.5";
+                        return $"{titleAttribute.Title} V1.6";
                     }
                 }
                 return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().CodeBase);
